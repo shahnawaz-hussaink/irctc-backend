1732e942b8f1023e417c2e481521125a0d829e49
shahnawaz-hussaink <shahnawaz.hussain96508@gmail.com>
2026-04-06 15:18:44 +0530

    feat: implement createPlatform controller and add route for platform creation

---
+++ src/controllers/platform.controller.js
@@ -0,0 +1,47 @@
+import prisma from "../db/prisma.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const createPlatform = asyncHandler(async (req, res) => {
+    const { platformNumber } = req.body;
+    const stationId  = parseInt(req.params.stationId) ;
+
+
+    if (!platformNumber || !stationId) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+
+    const isPlatfromExist = await prisma.platform.findFirst({
+        where: { AND: [{ platformNumber }, { stationId }] },
+    });
+
+    if (isPlatfromExist) {
+        throw new ApiError(400, "Platfrom Exists on this Station");
+    }
+
+    const platform = await prisma.platform.create({
+        data: {
+            platformNumber,
+            stationId,
+        },
+    });
+
+    if (!platform) {
+        throw new ApiError(400, "Something Went Wrong while Creating Platform");
+    }
+
+    return res
+        .status(201)
+        .json(
+            new ApiResponse(
+                201,
+                platform,
+                "Congratulations!!! Platform Created Successfully"
+            )
+        );
+});
+
+
+export {createPlatform}
--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { createStation } from "../controllers/station.controller.js";
+import { createPlatform } from "../controllers/platform.controller.js";
 
 const router = Router();
 
 router.route("/station").post(createStation);
+router.route("/stations/:stationId/platforms").post(createPlatform);
 
 export default router;
