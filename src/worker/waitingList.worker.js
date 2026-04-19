6e136e96d1be170469bf9d9d2c62b5daa9770de5
shahnawaz-hussaink <shahnawaz.hussain96508@gmail.com>
2026-04-19 18:37:03 +0530

    feat: refactor cancellation logic in waiting list worker; improve transaction handling and update passenger status; complete : waiting List and cancelllation of single or multi-tickets

--- src/worker/waitingList.worker.js
+++ src/worker/waitingList.worker.js
@@ -11,77 +11,81 @@ const waitingListWorker = new Worker(
         if (type === "CANCELLATION") {
             const { scheduleId, coachType } = data;
 
-            await prisma.$transaction(async (txn) => {
-                const schedule = await txn.schedule.findFirst({
-                    where: { id: scheduleId },
-                });
-                while (true) {
-                    const nextWaiting = await txn.passengerInfo.findFirst({
-                        where: {
-                            booking: {
-                                scheduleId: scheduleId,
-                                coachType: coachType,
-                            },
-                            passengerStatus: "WAITING",
-                        },
-                        include : {
-                            booking : {
-                                select : {userId : true }
-                            }
-                        },
-                        orderBy: { createdAt: "asc" },
+            await prisma.$transaction(
+                async (txn) => {
+                    const schedule = await txn.schedule.findFirst({
+                        where: { id: scheduleId },
                     });
-
-                    if (!nextWaiting) break;
-
-                    const seat = await txn.seat.findFirst({
-                        where: {
-                            coach: {
-                                trainId: schedule.trainId,
-                                coachType,
+                    while (true) {
+                        const nextWaiting = await txn.passengerInfo.findFirst({
+                            where: {
+                                booking: {
+                                    scheduleId: scheduleId,
+                                    coachType: coachType,
+                                },
+                                passengerStatus: "WAITING",
                             },
-                            seatLock: {
-                                none: {
-                                    scheduleId,
-                                    status: { in: ["HELD", "BOOKED"] },
+                            include: {
+                                booking: {
+                                    select: { userId: true },
                                 },
                             },
-                        },
-                        take: 1,
-                    });
+                            orderBy: { createdAt: "asc" },
+                        });
 
-                    if (!seat) {
-                        break;
+                        if (!nextWaiting) break;
+
+                        const seat = await txn.seat.findFirst({
+                            where: {
+                                coach: {
+                                    trainId: schedule.trainId,
+                                    coachType,
+                                },
+                                seatLock: {
+                                    none: {
+                                        scheduleId,
+                                        status: { in: ["HELD", "BOOKED"] },
+                                    },
+                                },
+                            },
+                            take: 1,
+                        });
+
+                        if (!seat) {
+                            break;
+                        }
+
+                        const updatedBooking = await txn.booking.updateMany({
+                            where: { id: nextWaiting.bookingId },
+                            data: { status: "PARTIAL_CONFIRMED" },
+                        });
+
+                        if (updatedBooking.count === 0) {
+                            continue;
+                        }
+
+                        const updatedSeatLock = await txn.seatLock.create({
+                            data: {
+                                userId: nextWaiting.booking.userId,
+                                seatId: seat.id,
+                                scheduleId: schedule.id,
+                                status: "BOOKED",
+                                heldUntil: getTenMinTime(),
+                                bookingId: nextWaiting.bookingId,
+                            },
+                        });
+
+                        await txn.passengerInfo.update({
+                            where: { id: nextWaiting.id },
+                            data: {
+                                passengerStatus: "CONFIRMED",
+                                seatLockId: updatedSeatLock.id,
+                            },
+                        });
                     }
-
-                    console.log(seat)
-
-                    await txn.passengerInfo.update({
-                        where: { id: nextWaiting.id },
-                        data : {passengerStatus : "CONFIRMED"}
-                    });
-
-                    const updatedBooking = await txn.booking.updateMany({
-                        where: { id: nextWaiting.bookingId },
-                        data: { status: "PARTIAL_CONFIRMED" },
-                    });
-
-                    if (updatedBooking.count === 0) {
-                        continue;
-                    }
-
-                    await txn.seatLock.create({
-                        data: {
-                            userId: nextWaiting.booking.userId,
-                            seatId: seat.id,
-                            scheduleId: schedule.id,
-                            status: "BOOKED",
-                            heldUntil: getTenMinTime(),
-                            bookingId: nextWaiting.bookingId,
-                        },
-                    });
-                }
-            },{ timeout: 30000 });
+                },
+                { timeout: 30000 }
+            );
 
             console.log("Waiting List workedd ");
         }
